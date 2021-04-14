77eb4dd86e1d4521efdb6514289a43b5cac57c6c
Marwan Khalid <33214208+marwankhalid@users.noreply.github.com>
2021-04-14 19:23:01 +0500

    Initial Commit

---
+++ Educator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Educator
+//
+//  Created by Marwan Khalid on 4/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
